6c970362f1eb532733b2067c025c5c32a1dd7178
ivanprofpv <akvabanka@gmail.com>
2023-08-30 14:33:13 +0400

    add detail view article page

--- news/views.py
+++ news/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView
 
 def news_home(request):
     news = Articles.objects.order_by('-date')[:5]
     return render(request, 'news/news_home.html', {'news': news})
+
 
 def create(request):
     error = ''
@@ -23,3 +25,8 @@ def create(request):
         'error': error
     }
     return render(request, 'news/create.html', data)
+
+class NewDetailsView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
