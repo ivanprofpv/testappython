76782b5d10db8e4eeb7aa6b8b06371fc2a1dcf7c
ivanprofpv <akvabanka@gmail.com>
2023-08-30 15:34:47 +0400

    add edit and delete button/mechanism

--- news/urls.py
+++ news/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.news_home, name='news_home'),
     path('create', views.create, name='create'),
-    path('<int:pk>', views.NewDetailsView.as_view(), name='news-detail')
+    path('<int:pk>', views.NewDetailsView.as_view(), name='news-detail'),
+    path('<int:pk>/update', views.NewUpdateView.as_view(), name='news-update'),
+    path('<int:pk>/delete', views.NewDeleteView.as_view(), name='news-delete')
 ]
--- news/views.py
+++ news/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
-from django.views.generic import DetailView
+from django.views.generic import DetailView, UpdateView, DeleteView
+
+class NewDetailsView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
+
+class NewUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/update.html'
+
+    form_class = ArticlesForm
+
+class NewDeleteView(DeleteView):
+    model = Articles
+    success_url = '/news/'
+    template_name = 'news/delete.html'
 
 def news_home(request):
     news = Articles.objects.order_by('-date')[:5]
@@ -25,8 +41,3 @@ def create(request):
         'error': error
     }
     return render(request, 'news/create.html', data)
-
-class NewDetailsView(DetailView):
-    model = Articles
-    template_name = 'news/details_view.html'
-    context_object_name = 'article'
